0f2f7db96e1fa94e48f56dea5ef7b5f163739ab5
AntonNarkevich <2648159780q>
2016-02-01 12:19:58 -0800

    Renames user investments to user contributions.

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/:apikey/investments', function(req, res, next) {
-  var sampleUserInvestments = require('../samples/user-investments.json');
+router.get('/:apikey/contributions', function(req, res, next) {
+  var sampleUserContributions = require('../samples/user-contributions.json');
 
-  res.send(sampleUserInvestments);
+  res.send(sampleUserContributions);
 });
 
 router.get('/:apikey/earnings', function(req, res, next) {
